fd95d6baabda53c4354cc127899559cd9ffa57dc
wahl <giovaniwahl@gmail.com>
2023-10-17 18:16:44 -0300

    implementação da entidade morador

--- src/main/java/com/api/parkingcontrol/models/ResidentModel.java
+++ src/main/java/com/api/parkingcontrol/models/ResidentModel.java
@@ -7,22 +7,24 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "TB_PARKING_SPOT")
-public class ParkingSpotModel implements Serializable {
+@Table(name = "TB_RESIDENT")
+public class ResidentModel implements Serializable {
     @Serial
     private static final long serialVersionUID=1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    @Column(nullable = false,unique = true,length = 10)
-    private String parkingSpotNumber;
-    private LocalDateTime registrationDate;
+    @Column(nullable = false,length = 130)
+    private String responsibleName;
+    @Column(nullable = false,length = 10)
+    private String apartment;
+    @Column(nullable = false,length = 10)
+    private String block;
 }
